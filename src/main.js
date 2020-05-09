4762a937364dc72e6212de412840c485e30689fc
Thomas Dunne <thomasdunne999@gmail.com>
2020-05-09 20:39:31 +0100

    Adding dotenv files to hold API KEYs

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import * as VueGoogleMaps from 'vue2-google-maps'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 
+require('dotenv').config()
+
 Vue.config.productionTip = false
 
 Vue.use(Buefy)
 Vue.use(VueGoogleMaps, {
     load: {
-        key: '***REMOVED***',
+        key: process.env.GOOGLE_API_KEY,
         libraries: 'places,drawing,geometry',
     },
     installComponents: true,
